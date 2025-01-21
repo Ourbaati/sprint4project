8f2e220111358af3810dfb4192fd0d563fc1d70a
ourbaati <youssef.ourbaati@gmail.com>
2025-01-21 13:22:01 -0500

    update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title("Vehicle Data Analysis")
 
 # Dropdown for column selection
 st.header("Select a Column")
-columns = ['price', 'model_year', 'model', 'odometer']
+columns = ['Price', 'Model Year', 'Model', 'Odometer']
 selected_column = st.selectbox("Select a column:", columns)
 
 # Display the selected column
